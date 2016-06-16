09805d2a50bcd330ae83e74751ec4d778478895f
pressstart101 <walterscott404@yandex.com>
2016-06-16 15:58:03 -0400

    done

--- OOPCardsSimpleViews/FISCardSwitcherViewController.m
+++ OOPCardsSimpleViews/FISCardSwitcherViewController.m
@@ -4,6 +4,10 @@
 
 @interface FISCardSwitcherViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *top;
+@property (weak, nonatomic) IBOutlet UILabel *middle;
+@property (weak, nonatomic) IBOutlet UILabel *bottom;
+
 @end
 
 @implementation FISCardSwitcherViewController
@@ -21,5 +25,35 @@
     self.tenOfHearts = [[FISCard alloc] initWithSuit:@"♥️"
                                                 rank:@"10"];
 }
+
+- (IBAction)one:(id)sender {
+    
+    self.top.text = @"♠️3";
+    self.middle.text = @"♠️3";
+    self.bottom.text = @"♠️3";
+}
+
+- (IBAction)two:(id)sender {
+    self.top.text = @"♣️4";
+    self.middle.text = @"♣️4";
+    self.bottom.text = @"♣️4";
+}
+
+
+- (IBAction)three:(id)sender {
+    self.top.text = @"♦️8";
+    self.middle.text = @"♦️8";
+    self.bottom.text = @"♦️8";
+}
+
+
+- (IBAction)four:(id)sender {
+    self.top.text = @"♥️10";
+    self.middle.text = @"♥️10";
+    self.bottom.text = @"♥️10";
+}
+
+
+
 
 @end
